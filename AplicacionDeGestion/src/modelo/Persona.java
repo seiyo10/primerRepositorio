9f2e66dcc569d6a7c67a98e315f02eda952b6f79
Sergio S. <seiyoser@gmail.com>
2021-03-05 10:07:16 +0100

    primer commit

---
+++ AplicacionDeGestion/src/modelo/Persona.java
@@ -0,0 +1,49 @@
+package modelo;
+
+public class Persona {
+	private String nombre;
+	private int edad;
+	private String direccion;
+	
+	// 
+	public Persona() {
+	}
+
+	public Persona(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Persona(String nombre, int edad) {
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+
+	public Persona(String nombre, int edad, String direccion) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.direccion = direccion;
+	}
+	
+	/**
+	 * 
+	 * @param yoqse
+	 * @param yoqse2
+	 * @return
+	 */
+	public int probando(String probando1, String probando2) {
+		return 20;
+	}
+	
+	public void probandoAlgo(){
+		
+	}
+	public void calculo() {
+		
+	}
+	
+	public String toString() {
+		return "paquito";
+	}
+	
+
+}
